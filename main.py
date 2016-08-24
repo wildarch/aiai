765fb67b42359f8fa0b530d5baf13714de069e90
wildarch <daandegraaf9@gmail.com>
2016-08-24 10:03:31 +0200

    Working!

--- main.py
+++ main.py
@@ -12,12 +12,21 @@ def get_px_values(image):
     for rows in image:
         for item in rows:
             vals.append(item.red)
-    return vals
+    #return np.array(vals).reshape(1, len(vals))
+    return np.array(vals).reshape(36,1)
 
 def color_to_repr(col):
-    if col == 'b': return [1, 0, 0]
-    elif col == 'w': return [0, 1, 0]
-    elif col == 'x': return [0, 0, 1]
+    if col == 'b': r = [1, 0, 0]
+    elif col == 'w': r = [0, 1, 0]
+    elif col == 'x': r = [0, 0, 1]
+    else: return ValueError
+    return np.array(r).reshape(3,1)
+
+def color_to_test_repr(col):
+    if col == 'b': return 0
+    elif col == 'w': return 1
+    elif col == 'x': return 2
+    return ValueError
 
 def img_from_path(path):
     return Image(filename=path)
@@ -35,7 +44,7 @@ training_data = list(zip(get_data_from_images(images), map(color_to_repr, colors
 test_files = list(map(lambda file: os.path.join(test_dir, file), os.listdir(test_dir)))
 colors = map(lambda file: file[-5], test_files)
 images = list(map(img_from_path, test_files))
-test_data = list(zip(get_data_from_images(images), map(color_to_repr, colors)))
+test_data = list(zip(get_data_from_images(images), map(color_to_test_repr, colors)))
 
 network = Network([img_width*img_width, 12, 3])
 network.SGD(training_data, 30, 10, 3.0, test_data=test_data)
--- stolen_network.py
+++ stolen_network.py
@@ -51,7 +51,7 @@ class Network(object):
         network will be evaluated against the test data after each
         epoch, and partial progress printed out.  This is useful for
         tracking progress, but slows things down substantially."""
-        if test_data: n_test = len(test_data)
+        if test_data is not None: n_test = len(test_data)
         n = len(training_data)
         for j in range(epochs):
             random.shuffle(training_data)
@@ -60,7 +60,7 @@ class Network(object):
                 for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
-            if test_data:
+            if test_data is not None:
                 print("Epoch {0}: {1} / {2}".format(
                     j, self.evaluate(test_data), n_test))
             else:
